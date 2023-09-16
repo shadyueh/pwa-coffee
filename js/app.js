beae8581e21bf58c3adb918e745ff5854b2693dc
Held Grijo <held.grijo@gmail.com>
2023-09-16 00:58:38 -0400

    feat: create a service worker

--- js/app.js
+++ js/app.js
@@ -26,4 +26,13 @@ const showCoffees = () => {
   container.innerHTML = output
 }
 
-document.addEventListener("DOMContentLoaded", showCoffees)
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
